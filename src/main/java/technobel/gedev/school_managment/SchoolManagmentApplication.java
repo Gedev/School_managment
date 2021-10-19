22a1325b0efe4e18c56d30ea7d6ecfc30a3d36cb
Gérald Devreux <gerald.devreux.pro@gmail.com>
2021-10-19 10:55:49 +0200

    Project initialization

---
+++ src/main/java/technobel/gedev/school_managment/SchoolManagmentApplication.java
@@ -0,0 +1,13 @@
+package technobel.gedev.school_managment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/technobel/gedev/school_managment/SchoolManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package technobel.gedev.school_managment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
